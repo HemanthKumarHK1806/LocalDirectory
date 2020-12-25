6a8626594b0e1a778daa90e5dcefeddf196b541a
Hemanth <hkhemanthkumar1806@gmail.com>
2020-12-24 22:21:03 -0800

    First Commit Pages

---
+++ STSDemo/src/main/java/com/hemanth/www/STSDemo/App.java
@@ -0,0 +1,13 @@
+package com.hemanth.www.STSDemo;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
